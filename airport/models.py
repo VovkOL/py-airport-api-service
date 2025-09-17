f8c78ffee8a33f434b120449f3051a6574a2da35
VovkOL <aav162807@gmail.com>
2025-09-17 16:12:25 -0500

    Add Crew model

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
