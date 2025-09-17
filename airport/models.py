4a679213838c4562995ff39ed7cda429b41f5b60
VovkOL <aav162807@gmail.com>
2025-09-17 16:18:50 -0500

    Add Order model with user and created_at

--- airport/models.py
+++ airport/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Crew(models.Model):
     first_name = models.CharField(max_length=100)
@@ -7,3 +9,8 @@ class Crew(models.Model):
 
 class AirplaneType(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    crated_at = models.DateTimeField(auto_now_add=True)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
