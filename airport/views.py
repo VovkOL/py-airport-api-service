443ff9c8ad50bd42f3a4a6dc1fb38aac9662ce3d
VovkOL <aav162807@gmail.com>
2025-09-29 15:52:48 -0500

    Add CrewViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import Crew
+from airport.serializers import CrewSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
