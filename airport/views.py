6f75aecb14a6966df6df28d74326b30be50474e2
VovkOL <aav162807@gmail.com>
2025-09-29 15:59:19 -0500

    Add AirplaneTypeViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
 
-from airport.models import Crew
-from airport.serializers import CrewSerializer
+from airport.models import Crew, AirplaneType
+from airport.serializers import CrewSerializer, AirplaneTypeSerializer
 
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
