e83b62b11f080659dde825853d68bf8ffb32fe89
VovkOL <aav162807@gmail.com>
2025-10-03 13:36:07 -0500

    Add RouteViewSet

--- airport/views.py
+++ airport/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
 
-from airport.models import Crew, AirplaneType, Airport, Airplane
-from airport.serializers import CrewSerializer, AirplaneTypeSerializer, AirportSerializer, AirplaneSerializer
+from airport.models import Crew, AirplaneType, Airport, Airplane, Route
+from airport.serializers import CrewSerializer, AirplaneTypeSerializer, AirportSerializer, AirplaneSerializer, \
+    RouteSerializer, RouteListSerializer, RouteDetailSerializer
 
 
 class CrewViewSet(viewsets.ModelViewSet):
@@ -35,3 +36,29 @@ class AirplaneViewSet(
 ):
     queryset = Airplane.objects.select_related("airplane_type")
     serializer_class = AirplaneSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related("source", "destination")
+    serializer_class = RouteSerializer
+
+    def get_queryset(self):
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+
+        queryset = self.queryset
+
+        if source:
+            queryset = queryset.filter(source__name__icontains=source)
+        if destination:
+            queryset = queryset.filter(destination__name__icontains=destination)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+
+        return RouteSerializer
