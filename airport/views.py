b33af43dff3808f655f31f2c88fdef6fde3db880
VovkOL <aav162807@gmail.com>
2025-10-08 14:35:04 -0500

    Add FlightViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,10 +1,23 @@
+from datetime import datetime
+
+from django.db.models import F, Count
 from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
 
-from airport.models import Crew, AirplaneType, Airport, Airplane, Route
-from airport.serializers import CrewSerializer, AirplaneTypeSerializer, AirportSerializer, AirplaneSerializer, \
-    RouteSerializer, RouteListSerializer, RouteDetailSerializer
+from airport.models import Crew, AirplaneType, Airport, Airplane, Route, Flight
+from airport.serializers import (
+    CrewSerializer,
+    AirplaneTypeSerializer,
+    AirportSerializer,
+    AirplaneSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    FlightSerializer,
+    FlightListSerializer,
+    FlightDetailSerializer
+)
 
 
 class CrewViewSet(viewsets.ModelViewSet):
@@ -62,3 +75,42 @@ class RouteViewSet(viewsets.ModelViewSet):
             return RouteDetailSerializer
 
         return RouteSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Flight.objects.all()
+        .select_related("route", "airplane")
+        .prefetch_related("crew")
+        .annotate(
+            tickets_available=(
+                F("airplane__rows") * F("airplane__seats_in_row")
+                - Count("tickets")
+            )
+        )
+    )
+    serializer_class = FlightSerializer
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        route_id_str = self.request.query_params.get("route")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(departure_time__date=date)
+
+        if route_id_str:
+            queryset = queryset.filter(route__id=int(route_id_str))
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FlightListSerializer
+
+        if self.action == "retrieve":
+            return FlightDetailSerializer
+
+        return FlightSerializer
