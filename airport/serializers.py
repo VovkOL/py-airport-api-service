6442403832876bfd80d0363b66fb45d94a59e24c
VovkOL <aav162807@gmail.com>
2025-10-09 16:23:26 -0500

    Add OrderListSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -162,3 +162,7 @@ class OrderSerializer(serializers.ModelSerializer):
             for ticket_data in tickets_data:
                 Ticket.objects.create(order=order, **ticket_data)
             return order
+
+
+class OrderListSerializer(OrderSerializer):
+    tickets = TicketListSerializer(many=True, read_only=True)
