66135616446495a360c23503bab7b82c6f9e0b97
VovkOL <aav162807@gmail.com>
2025-10-10 16:14:56 -0500

    Add airport app URL configuration with routers

---
+++ airport/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    CrewViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    AirportViewSet,
+    RouteViewSet,
+    FlightViewSet,
+    OrderViewSet,
+)
+
+app_name = "airport"
+
+router = routers.DefaultRouter()
+
+router.register("crews", CrewViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airports", AirportViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("routes", RouteViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
